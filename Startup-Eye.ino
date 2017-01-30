49c2d38ad57ecf0d9e3c704d8bc6a59284d0fe00
jondavis401 <jrd74186@gmail.com>
2017-01-30 16:57:10 +1000

    Added K9-Scripts

---
+++ Startup-Eye.ino
@@ -0,0 +1,26 @@
+void setup() {
+pinMode(12,OUTPUT);
+startupeye();
+startupeye();
+}
+
+void loop() {
+
+digitalWrite(12,HIGH);
+delay(500);
+digitalWrite(12,LOW);
+delay(500);
+}
+
+void startupeye() {
+  digitalWrite(12,HIGH);
+delay(100);
+digitalWrite(12,LOW);
+delay(100);
+  digitalWrite(12,HIGH);
+delay(200);
+digitalWrite(12,LOW);
+delay(150);
+  digitalWrite(12,HIGH);
+delay(500); }
+
